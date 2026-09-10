2edd44bdc0b8aeab91f285e8f11f4e468067e03a
Mundo bit Byte <mundobitbyte@gmail.com>
2026-09-10 16:32:46 -0300

    Inclui Projeto IoT no menu do Bloco 5

--- js/arduino-programacao-aplicada.js
+++ js/arduino-programacao-aplicada.js
@@ -53,6 +53,15 @@ document.addEventListener('DOMContentLoaded', function(){
     if(exercicios) menu.insertBefore(link9, exercicios);
     else menu.appendChild(link9);
   }
+
+  if(!menu.querySelector('a[href="arduino-projeto-iot.html"]')){
+    const link10 = document.createElement('a');
+    link10.className = 'module-btn';
+    link10.href = 'arduino-projeto-iot.html';
+    link10.textContent = '10. Projeto IoT';
+    if(exercicios) menu.insertBefore(link10, exercicios);
+    else menu.appendChild(link10);
+  }
 });
 
 function copyCode(id, button){
