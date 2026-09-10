a4517ae5efbced0d39218c5c23c4ed64dca27f62
Mundo bit Byte <mundobitbyte@gmail.com>
2026-09-10 18:59:29 -0300

    Exibe diagramas de RTOS e I2C no bloco de protocolos

--- js/arduino-protocolos.js
+++ js/arduino-protocolos.js
@@ -137,3 +137,9 @@ function manterModuloAtivoVisivel(){
 document.addEventListener('DOMContentLoaded', manterModuloAtivoVisivel);
 window.addEventListener('pageshow', manterModuloAtivoVisivel);
 window.addEventListener('resize', manterModuloAtivoVisivel);
+
+(() => {
+  const script = document.createElement('script');
+  script.src = '../js/arduino-circuitos-visuais.js?v=20260910-1';
+  document.head.appendChild(script);
+})();
