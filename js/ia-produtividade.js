f8f0d40fc2f80d6e74e12615b63d974fd80e7ff0
Mundo bit Byte <mundobitbyte@gmail.com>
2026-09-12 12:34:34 -0300

    Integra navegação lateral em IA e Produtividade

--- js/ia-produtividade.js
+++ js/ia-produtividade.js
@@ -16,3 +16,13 @@ function copyPrompt(button) {
     }, 1400);
   });
 }
+
+// Mantém a navegação dos módulos 3 a 6 coerente com a trilha dos módulos iniciais.
+(() => {
+  if (document.querySelector('script[data-ia-trilha-modulos]')) return;
+  const script = document.createElement('script');
+  script.src = '../../js/ia-trilha-modulos.js?v=1';
+  script.defer = true;
+  script.dataset.iaTrilhaModulos = '1';
+  document.head.appendChild(script);
+})();
