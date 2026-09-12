17bc2673651538b757ddf12ea36f8668213691ed
Mundo bit Byte <mundobitbyte@gmail.com>
2026-09-12 12:34:50 -0300

    Integra navegação lateral em IA e Programação

--- js/ia-programacao.js
+++ js/ia-programacao.js
@@ -133,3 +133,13 @@ dados/equipamentos.json</pre>
     else proximo.appendChild(link);
   }
 });
+
+// Mantém a navegação dos módulos 3 a 6 coerente com a trilha dos módulos iniciais.
+(() => {
+  if (document.querySelector('script[data-ia-trilha-modulos]')) return;
+  const script = document.createElement('script');
+  script.src = '../../js/ia-trilha-modulos.js?v=1';
+  script.defer = true;
+  script.dataset.iaTrilhaModulos = '1';
+  document.head.appendChild(script);
+})();
