719b44576bae524fd12242e11ea6e6f578619270
Mundo bit Byte <mundobitbyte@gmail.com>
2026-09-22 10:20:31 -0300

    Atualiza cache do checkpoint Git GitHub

--- js/git-mbb-checkpoint-comandos.js
+++ js/git-mbb-checkpoint-comandos.js
@@ -1,7 +1,7 @@
 (() => {
   if (!modules.comandos) return;
 
-  const checkpointUrl = '../assets/git-github-checkpoint-comandos.txt';
+  const checkpointUrl = '../assets/git-github-checkpoint-comandos.txt?v=2';
   const steps = modules.comandos.steps;
 
   if (!steps.some(step => String(step.id) === 'checkpoint')) {
