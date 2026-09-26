f8faaabe8248e5538a36f3e03573dfdf0255e0be
Mundo bit Byte <mundobitbyte@gmail.com>
2026-09-25 21:34:14 -0300

    React Native: ativar nova estrutura de Fundamentos

---
+++ js/reactnative-livro-dados-cap1b.js
@@ -0,0 +1,8 @@
+// Entrada de compatibilidade já referenciada por pages/reactnative.html.
+// Carrega por último a reconstrução do módulo 0. Fundamentos no padrão MbB.
+(function () {
+  const script = document.createElement('script');
+  script.src = '../js/reactnative-fundamentos-mbb-20260925.js?v=20260925-1';
+  script.async = false;
+  document.head.appendChild(script);
+})();
